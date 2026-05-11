f6a4ba4732e6e737f10eee896edeecd7f055079a
Braxton Anderson <braxtonaan@gmail.com>
2026-05-10 20:50:32 -0600

    update rootpath

--- utils.m
+++ utils.m
@@ -4,7 +4,7 @@
 NSString *localizedStringForKey(NSString *key) {
     static NSBundle *tweakBundle;
     if (!tweakBundle)
-        tweakBundle = [[NSBundle alloc] initWithPath:@ROOT_PATH("/Library/Application Support/SettingsWidgets")];
+        tweakBundle = [[NSBundle alloc] initWithPath:ROOT_PATH_NS(@"/Library/Application Support/SettingsWidgets")];
 
     return [tweakBundle localizedStringForKey:key value:@"" table:nil];
 }
